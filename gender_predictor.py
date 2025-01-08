1518fa7c4ff55f0ea8c6eb34c480f141f9344066
Sakshi <80853387+sakshixgoel@users.noreply.github.com>
2025-01-08 00:27:47 -0500

    Add files via upload

---
+++ gender_predictor.py
@@ -0,0 +1,11 @@
+from sklearn import tree
+# height, weight, shoe size
+X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40], [190, 90, 47], [175, 64, 39], [177, 70, 40], [171, 75, 42], [181, 85, 43], [190, 87, 43]]
+Y = ['male', 'female', 'female', 'female', 'female', 'male', 'female', 'female', 'female', 'male', 'male' ]
+#classifier 
+clf = tree.DecisionTreeClassifier()
+
+clf = clf.fit(X, Y)
+prediction = clf.predict([[190, 70, 43]])
+
+print prediction
